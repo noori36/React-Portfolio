97d2da2692bacbb8dbb35903f48dc5548ac2f8cb
Noreen Arshads <noreen@Noreens-MBP.lan>
2022-10-04 11:43:20 -0700

    about page and  nav bar

---
+++ src/components/Header/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Navigation from "../../components/Navigation";
+import About from "../../components/About";
+import Portfolio from "../../components/Portfolio";
+import Contact from '../../components/Contact';
+import Resume from '../../components/Resume';
+
+
+class Header extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <header>
+          <Navigation />
+        </header>
+
+        <div className="content">
+          <Route exact path="/" render={() => <Redirect to="/about" />} />
+          <Route path="/about" component={About} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact}/>
+          <Route path="/resume" component={Resume}/>
+        </div>
+      </HashRouter>
+    );
+  }
+}
+
+export default Header;
